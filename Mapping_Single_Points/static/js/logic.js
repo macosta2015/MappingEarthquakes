db162ff205074ad9849795df222168e57839d62f
macosta2015 <macosta2015@my.fit.edu>
2021-10-08 19:34:36 -0400

    Changing the marker
    We are changing the marker to become a circle. We use the following code:

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -6,8 +6,13 @@ console.log("working");
 // Create the map object with a center and zoom level.
 let map = L.map('mapid').setView([40.7, -94.5], 4);
 
-//  Add a marker to the map for Los Angeles, California.
+// Add a marker to the map for Los Angeles, California.
 let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+// Add a marker to the map for Los Angeles, California 
+L.circle([34.0522, -118.2437], {
+    radius: 100
+ }).addTo(map);
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
