dacc7a449e9a5103b257d06df672a1577d8aa4fb
macosta2015 <macosta2015@my.fit.edu>
2021-10-09 14:32:50 -0400

    Adding the maps radius
    We are going to add the map radius

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -62,9 +62,9 @@ let cityData = cities;
 // Loop through the cities array and create one marker for each city.
 cityData.forEach(function(city) {
     console.log(city)
-    L.marker(city.location)
+    L.circleMarker(city.location, {radius: city.population/100000})
     .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-  .addTo(map);
+    .addTo(map);
 });
 
 // Add a marker to the map for Los Angeles, California 
