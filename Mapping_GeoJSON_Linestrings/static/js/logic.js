165b7474eb4cfc546791fa2601a82976fc382cd5
macosta2015 <macosta2015@my.fit.edu>
2021-10-10 11:08:37 -0400

    Pushing

--- Mapping_GeoJSON_Linestrings/static/js/logic.js
+++ Mapping_GeoJSON_Linestrings/static/js/logic.js
@@ -1,148 +1,18 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-///DON'T KNOW IF THIS GOES HERE
-// Create the map object with a center and zoom level.
-//let map = L.map('mapid').setView([40.7, -94.5], 4);
-//7 is the radius
-//Line is for setting the zoom level
-//let map = L.map('mapid').setView([36.1733, -120.1794], 7);
-// Create the map object with center at the San Francisco airport.
-//let map = L.map('mapid').setView([37.6213, -122.3790], 5);
-//We are going to be working with the San Francisco aiport, so we chose it here
-// Create the map object with center at the San Francisco airport.
-// let map = L.map('mapid').setView([37.5, -122.5], 10);
-///We are going to change the view again. Zoom will noe be to the center of the Earth
-// Create the map object with center and zoom level.
+//Addin the Toronto part
+// Accessing the Toronto airline routes GeoJSON URL.
+let torontoData = "https://raw.githubusercontent.com/macosta2015/Mapping_Earthquakes/main/torontoRoutes.json";
 let map = L.map('mapid').setView([30, 30], 2);
 
-/////////////////////////////////
-// // Add GeoJSON data.
-// let sanFranAirport =
-// {"type":"FeatureCollection","features":[{
-//     "type":"Feature",
-//     "properties":{
-//         "id": "3469",
-//         "name":"San Francisco International Airport",
-//         "city":"San Francisco",
-//         "country":"United States",
-//         "faa":"SFO",
-//         "icao":"KSFO",
-//         "alt":"13",
-//         "tz-offset":"-8",
-//         "dst":"A",
-//         "tz":"America/Los_Angeles"},
-//         "geometry":{
-//             "type":"Point",
-//             "coordinates":[-122.375,37.61899948120117]}}
-// ]};
+d3.json(torontoData).then(function(data) {
+  console.log(data);
+// Creating a GeoJSON layer with the retrieved data.
+L.geoJson(data).addTo(map);
+});
 
-// Grabbing our GeoJSON data.
-//L.geoJSON(sanFranAirport).addTo(map);
-///Adding more information into the GeoJason
-// Grabbing our GeoJSON data.
-// L.geoJson(sanFranAirport, {
-//   // We turn each feature into a marker on the map.
-//   pointToLayer: function(feature, latlng) {
-//     console.log(feature);
-//     //We added two popups for testing purposes!
-//     return L.marker(latlng).bindPopup(("<h2>" + feature.properties.city + "</h2>")+("<h2>" + feature.properties.city + "</h2>"))
-//   }
-// }).addTo(map);
-/// We keep editing and learning
-//////////////////////////////////////////////
-// L.geoJson(sanFranAirport, {
-//   onEachFeature: function(feature, layer) {
-//     console.log(layer);
-//     layer.bindPopup();
-//    }
-// }).addTo(map);
-
-
-// Coordinates for each point to be used in the polyline.
-//This are the airports coordinates
-// let line = [
-//     [33.9416, -118.4085],
-//     [37.6213, -122.3790],
-//     [40.7899, -111.9791],
-//     [47.4502, -122.3088]
-//   ];
-  
-
-// // Create a polyline using the line coordinates and make the line red.
-// L.polyline(line, {
-//     color: "yellow"
-//   }).addTo(map);
-  
-// Add a marker to the map for Los Angeles, California.
-//let marker = L.marker([34.0522, -118.2437]).addTo(map);
-///We are replacing the above code to the below one
-// // An array containing each city's location, state, and population.
-// let cities = [{
-//     location: [40.7128, -74.0059],
-//     city: "New York City",
-//     state: "NY",
-//     population: 8398748
-//   },
-//   {
-//     location: [41.8781, -87.6298],
-//     city: "Chicago",
-//     state: "IL",
-//     population: 2705994
-//   },
-//   {
-//     location: [29.7604, -95.3698],
-//     city: "Houston",
-//     state: "TX",
-//     population: 2325502
-//   },
-//   {
-//     location: [34.0522, -118.2437],
-//     city: "Los Angeles",
-//     state: "CA",
-//     population: 3990456
-//   },
-//   {
-//     location: [33.4484, -112.0740],
-//     city: "Phoenix",
-//     state: "AZ",
-//     population: 1660272
-//   }
-//   ];
-
-
-// Get data from cities.js
-/// When handling large databases, it's a best pratice to have the data in an external 
-//file and refer to that file and dataset in the logic.js
 let cityData = cities;
-
-// // Loop through the cities array and create one marker for each city.
-// cities.forEach(function(city) {
-//     console.log(city)
-//     L.marker(city.location).addTo(map);
-//    });
-///We are replacing the above code to a foor loop 
-// Loop through the cities array and create one marker for each city.
-// cityData.forEach(function(city) {
-//     console.log(city)
-//     L.marker(city.location).addTo(map);
-// });
-///Above code is wrong, will use the below one
-// Loop through the cities array and create one marker for each city.
-// cityData.forEach(function(city) {
-//     console.log(city)
-//     L.circleMarker(city.location, {radius: city.population/100000})
-//     .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-//     .addTo(map);
-// });
-
-//Taking below out
-// // Add a marker to the map for Los Angeles, California 
-// L.circle([34.0522, -118.2437], {
-//     radius: 300,
-//     color: "red",
-//     fillColor: 'fffa1'
-//  }).addTo(map);
 
 // We create the tile layer that will be the background of our map.
 // let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -154,10 +24,6 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
-
-// Accessing the airport GeoJSON URL
- let airportData = "https://raw.githubusercontent.com/macosta2015/Mapping_Earthquakes/main/majorAirports.json";
-//let airportData = "https://github.com/macosta2015/MappingEarthquakes/blob/50fab0bb43d8dc4f206217538cf7fe168b70d803/majorAirports.json";
 
 // Grabbing our GeoJSON data.
 d3.json(airportData).then(function(data) {
